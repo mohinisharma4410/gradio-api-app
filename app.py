168647e0afae63dae03902d761749459b5514aab
Mohini Sharma <111170118+mohinisharma4410@users.noreply.github.com>
2026-03-18 13:00:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -520,5 +520,5 @@ if __name__ == "__main__":
 
 #     demo.queue().launch(
 #         server_name="0.0.0.0",
-#         server_port=port
+#         server_port=int(os.environ.get("PORT", 7860)
 #     )
